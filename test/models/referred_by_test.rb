a2af4304365bf3aca919c76cfa26a594a079f5fb
lingtran <lxiutran@gmail.com>
2016-03-29 15:19:22 -0600

    added tests for models models are event_name, ip, parameter, referred by tests passed successfully

--- test/models/referred_by_test.rb
+++ test/models/referred_by_test.rb
@@ -5,6 +5,12 @@ class ReferredByTest < Minitest::Test
 
   def test_referred_by_class_can_be_created
     assert ReferredBy.create({:root => "www.google.com", :path => "/gmail"})
+    refute ReferredBy.new.valid?
+  end
+
+  def test_referred_by_has_payload_requests
+    referred_by = create_referred_by("www.google.com", "/gmail")
+    assert_respond_to referred_by, :payload_requests
   end
 
 end
