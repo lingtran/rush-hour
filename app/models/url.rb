e177c408b050cd57c40667035ab3e016373c7614
Drew Thompson <ferraritmnt@gmail.com>
2016-03-30 14:17:22 -0600

    finished iteration 2

--- app/models/url.rb
+++ app/models/url.rb
@@ -26,4 +26,42 @@ class Url < ActiveRecord::Base
     split[:path] = "/#{initial_split[1]}"
     split
   end
+
+  def self.all_response_times_for_url_ordered(url)
+    Url.get_responded_ins_for_url(url).sort.reverse
+  end
+
+  def self.average_response_time_by_url(url)
+    response_times = Url.get_responded_ins_for_url(url)
+    response_times.reduce(:+)/response_times.count
+  end
+
+  def self.http_verbs_for_url(url)
+    split = Url.url_parser(url)
+    result = self.find_by(:root => split[:root], :path => split[:path])
+    PayloadRequest.where(:url_id => result.id).map do |pr|
+      pr.request_type.verb
+    end.uniq
+  end
+
+  def self.three_most_popular_referrers(url)
+    split = Url.url_parser(url)
+    referred_bies = Hash.new(0)
+    result = self.find_by(:root => split[:root], :path => split[:path])
+    PayloadRequest.where(:url_id => result.id).reduce(0)do |sum, pr|
+      referred_bies[pr.referred_by.root + pr.referred_by.path] += 1
+    end
+    referred_bies.sort_by { |k,v| v }.reverse.take(3).map { |x| x.first }
+  end
+
+  def self.three_most_popular_user_agents(url)
+    split = Url.url_parser(url)
+    user_agents = Hash.new(0)
+    result = self.find_by(:root => split[:root], :path => split[:path])
+
+    PayloadRequest.where(:url_id => result.id).reduce(0)do |sum, pr|
+      user_agents["#{pr.user_agent.os} #{pr.user_agent.browser}"] += 1
+    end
+    user_agents.sort_by { |k,v| v }.reverse.take(3).map { |x| x.first }
+  end
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -49,9 +49,74 @@ class UrlTest < Minitest::Test
   end
 
   def test_average_response_time_by_url
-    create_payload_requests(3)
-    create_payload_requests(3)
-    create_payload_requests(3)
-    assert_equal 1, Url.average_response_time_by_url("google.com/search1")
+    create_payload_requests
+    create_payload_requests
+    PayloadRequest.create({
+      :url_id       => create_url("google.com", "/search1").id,
+      :requested_at => Date.new(2016, 01, 01),
+      :responded_in_id => create_responded_in(4).id,
+      :referred_by_id => create_referred_by("bing.com", "/search1").id,
+      :request_type_id => create_request_type("GET").id,
+      :event_name_id => create_event_name("eventName 3").id,
+      :user_agent_id => create_user_agent("OSX3", "Chrome 3").id,
+      :resolution_id => create_resolution("resolutionWidth 3", "resolutionHeight 3").id,
+      :ip_id => create_ip("127.0.0.3").id
+      })
+
+    assert_equal 2, Url.average_response_time_by_url("google.com/search1")
+  end
+
+  def test_all_response_times_for_url_are_ordered
+    create_payload_requests
+    PayloadRequest.create({
+      :url_id       => create_url("google.com", "/search1").id,
+      :requested_at => Date.new(2016, 01, 01),
+      :responded_in_id => create_responded_in(3).id,
+      :referred_by_id => create_referred_by("bing.com", "/search1").id,
+      :request_type_id => create_request_type("GET").id,
+      :event_name_id => create_event_name("eventName 3").id,
+      :user_agent_id => create_user_agent("OSX3", "Chrome 3").id,
+      :resolution_id => create_resolution("resolutionWidth 3", "resolutionHeight 3").id,
+      :ip_id => create_ip("127.0.0.3").id
+      })
+
+    assert_equal [3,1], Url.all_response_times_for_url_ordered("google.com/search1")
+  end
+
+  def test_all_http_verbs_by_url
+    create_payload_requests(2)
+    PayloadRequest.create({
+      :url_id       => create_url("google.com", "/search1").id,
+      :requested_at => Date.new(2016, 01, 01),
+      :responded_in_id => create_responded_in(1).id,
+      :referred_by_id => create_referred_by("bing.com", "/search1").id,
+      :request_type_id => create_request_type("POST").id,
+      :event_name_id => create_event_name("eventName").id,
+      :user_agent_id => create_user_agent("OSX1", "Chrome ").id,
+      :resolution_id => create_resolution("resolutionWidth ", "resolutionHeight ").id,
+      :ip_id => create_ip("127.0.0.32").id
+      })
+
+    assert_equal ["GET", "POST"], Url.http_verbs_for_url("google.com/search1")
+  end
+
+  def test_three_most_popular_referrers
+    create_payload_specific_url(4)
+    create_payload_specific_url(3)
+    create_payload_specific_url(2)
+    create_payload_specific_url
+
+    result = ["bing.com/search1", "bing.com/search2", "bing.com/search3"]
+    assert_equal result, Url.three_most_popular_referrers("google.com/search1")
+  end
+
+  def test_three_most_popular_user_agents
+    create_payload_specific_url(4)
+    create_payload_specific_url(3)
+    create_payload_specific_url(2)
+    create_payload_specific_url
+
+    result = ["OSX1 Chrome 1", "OSX2 Chrome 2", "OSX3 Chrome 3"]
+    assert_equal result, Url.three_most_popular_user_agents("google.com/search1")
   end
 end
