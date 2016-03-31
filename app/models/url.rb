2c189d9a40a1a20a8805bf664e208adac8acee1b
lingtran <lxiutran@gmail.com>
2016-03-30 20:30:22 -0600

    wip

--- app/models/url.rb
+++ app/models/url.rb
@@ -7,8 +7,8 @@ class Url < ActiveRecord::Base
     get_responded_ins_for_url.max
   end
 
-  def self.min_response_time_by_url(url)
-    Url.get_responded_ins_for_url(url).min
+  def min_response_time_by_url
+    get_responded_ins_for_url.min
   end
 
   def get_responded_ins_for_url
@@ -20,11 +20,15 @@ class Url < ActiveRecord::Base
     payload_requests.map { |pr| pr.responded_in.responded_in }
   end
 
-  def self.url_parser(url)
-    split = Hash.new
-    initial_split = url.split("/")
-    split[:root] = initial_split[0]
-    split[:path] = "/#{initial_split[1]}"
-    split
+  def average_response_time_by_url
+    # payload_requests.
   end
+
+  # def self.url_parser(url)
+  #   split = Hash.new
+  #   initial_split = url.split("/")
+  #   split[:root] = initial_split[0]
+  #   split[:path] = "/#{initial_split[1]}"
+  #   split
+  # end
 end
