e6ff2af308601a68939550a92f762c210a06d973
lingtran <lxiutran@gmail.com>
2016-04-02 16:19:59 -0600

    still wip for client_test

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,17 +2,15 @@
 module RushHour
   class Client < ActiveRecord::Base
     has_many :payload_requests
-
-    has_many :responded_ins, through: :payload_requests
     has_many :request_types, through: :payload_requests
-    has_many :referred_bies, through: :payload_requests
-    has_many :user_agents, through: :payload_requests
+
+    # has_many :responded_ins, through: :payload_requests
+    # has_many :request_types, through: :payload_requests
+    # has_many :referred_bies, through: :payload_requests
+    # has_many :user_agents, through: :payload_requests
 
     validates :identifier, presence: true, uniqueness: true
     validates :rootUrl, presence: true
   end
 
-  def self.response_time_all_requests
-    self.payload_requests.average_response_time
-  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -21,25 +21,39 @@ module RushHour
     end
 
     def test_can_get_average_response_time_for_all_requests
-      skip
+      # skip
       create_data
+      client_one_avg_response_t = @client1.payload_requests.average_response_time
+      client_two_avg_response_t = @client2.payload_requests.average_response_time
+      assert_equal 61.5, client_one_avg_response_t.to_f.round(2)
+      assert_equal 63.0, client_two_avg_response_t.to_f.round(2)
     end
 
     def test_can_get_max_response_time_across_all_requests
       # Max Response time across all requests
-      skip
+      # skip
+      create_data
+      client_one_max_response_t = @client1.payload_requests.max_response_time
+      client_two_max_response_t = @client2.payload_requests.max_response_time
+      assert_equal 62, client_one_max_response_t
+      assert_equal 65, client_two_max_response_t
     end
 
     def test_can_get_min_response_time_across_all_requests
       # Min Response time across all requests
-      skip
-
+      # skip
+      create_data
+      client_one_min_response_t = @client1.payload_requests.min_response_time
+      client_two_min_response_t = @client2.payload_requests.min_response_time
+      assert_equal 62, client_one_min_response_t
+      assert_equal 60, client_two_min_response_t
     end
 
     def test_can_get_most_frequent_request_type
       # Most frequent request type
-      skip
-
+      create_data
+      client_one_freq_request_type = @client1.request_types.most_frequent_request_type
+      assert_equal "POST", client_one_freq_request_type
     end
 
     def test_can_get_list_of_all_HTTP_verbs_used
@@ -50,7 +64,18 @@ module RushHour
 
     def test_can_get_list_of_urls_listed_from_most_requested_to_least_requested
       # List of URLs listed form most requested to least requested
+      # the URLs are not currently ordered
       skip
+      create_data
+      client_one_list_of_urls = @client1.payload_requests.list_of_urls
+      client_two_list_of_urls = @client2.payload_requests.list_of_urls
+
+      client_one_result = ["jumpstartlab.com/blog", "jumpstartlab.com/exam"]
+      # for client1 should be ["jumpstartlab.com/exam", "jumpstartlab/blog"]
+      client_two_result = ["jumpstartlab.com/blog", "jumpstartlab.com/home"]
+      # for client2 should be ["jumpstartlab.com/home", "jumpstartlab/blog"]
+      assert_equal client_one_result, client_one_list_of_urls
+      assert_equal client_two_result, client_two_list_of_urls
 
     end
 
