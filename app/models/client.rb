40b1c25799898b8c6f547717d1090c0d42376a1b
lingtran <lxiutran@gmail.com>
2016-04-02 15:12:59 -0600

    started creating tests for client with analytics

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,11 @@ module RushHour
   class Client < ActiveRecord::Base
     has_many :payload_requests
 
+    has_many :responded_ins, through: :payload_requests
+    has_many :request_types, through: :payload_requests
+    has_many :referred_bies, through: :payload_requests
+    has_many :user_agents, through: :payload_requests
+
     validates :identifier, presence: true, uniqueness: true
     validates :rootUrl, presence: true
   end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -10,7 +10,7 @@ module RushHour
     end
 
     def test_client_has_payload_requests
-      client = create_client("jumpstartlab", "jumpstartlab.com")
+      client = create_data
       assert_respond_to client, :payload_requests
     end
 
@@ -21,6 +21,57 @@ module RushHour
     end
 
     def test_can_get_average_response_time_for_all_requests
-      
+      skip
+      create_data
+    end
+
+    def test_can_get_max_response_time_across_all_requests
+      # Max Response time across all requests
+      skip
+    end
+
+    def test_can_get_min_response_time_across_all_requests
+      # Min Response time across all requests
+      skip
+
+    end
+
+    def test_can_get_most_frequent_request_type
+      # Most frequent request type
+      skip
+
+    end
+
+    def test_can_get_list_of_all_HTTP_verbs_used
+      # List of all HTTP verbs used
+      skip
+
+    end
+
+    def test_can_get_list_of_urls_listed_from_most_requested_to_least_requested
+      # List of URLs listed form most requested to least requested
+      skip
+
+    end
+
+    def test_can_get_web_browser_breakdown_across_all_requests
+      # Web browser breakdown across all requests
+      skip
+
+    end
+
+    def test_can_get_os_breakdown_across_all_requests
+      # OS breakdown across all requests
+      skip
+
+    end
+
+    def test_can_get_screen_resolutions_acrosss_all_requests
+      # Screen Resolutions across all requests (resolutionWidth x resolutionHeight)
+      skip
+
+    end
+
+
   end
 end
